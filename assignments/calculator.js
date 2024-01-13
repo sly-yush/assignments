ab8eefc93bc4b692f45f4a9663e2504f8b1779da
sly_yush <sly.yush@gmail.com>
2024-01-13 17:14:15 -0500

    created calculator with 4 functions

---
+++ assignments/calculator.js
@@ -0,0 +1,19 @@
+// Function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Function to subtract the second number from the first
+function subtract(a, b) {
+    return a - b;
+}
+
+// Function to divide the first number by the second (check for division by zero)
+function divide(a, b) {
+    return a / b;
+}
